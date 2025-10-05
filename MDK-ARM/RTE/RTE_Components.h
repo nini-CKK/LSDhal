a36961ac7229bd2410cbbb813f9c89392b5c2a7d
Danny_test <19823297156@163.com>
2025-10-05 22:37:20 +0800

    MDK-ARM

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32_HAL_LSD' 
+ * Target:  'STM32_HAL_LSD' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
